6893ab5c3c677d1b0c4194a1849185af6c928c73
susmitha9542 <susmitha.tinku@gmail.com>
2020-01-31 15:05:28 -0500

    mailers concept

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,9 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
-  layout 'mailer'
+  #layout 'mailer'
+  def send_email
+    @user = params[:user]
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
 end
