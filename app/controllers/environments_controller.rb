02159f2d48e85fbf5cbc45b3cdf8e77efa462d34
susmitha9542 <susmitha.tinku@gmail.com>
2020-01-31 15:40:08 -0500

    Reports fix

--- app/controllers/environments_controller.rb
+++ app/controllers/environments_controller.rb
@@ -80,8 +80,20 @@ class EnvironmentsController < ApplicationController
   end
 
   def reports
-    @test_suites = TestSuite.all
-    @schedule = Scheduler.all
+    logger.debug("SESSION OBJECT #{session[:enviro_id].inspect}")
+    id = session[:enviro_id]
+    @test_suites = TestSuite.where(environment_id: id).pluck(:id)
+    if @test_suites.present?
+      logger.debug(" TEST SUITE #{@test_suites} ")
+      @test_suites.each do |ts_id|
+        @schedule = Array.new
+        sch = Scheduler.where(test_suite_id: ts_id).pluck(:id)
+        sch.each do |id|
+          @schedule << Scheduler.find(id)
+          logger.debug(" TEST SUITE #{@schedule.inspect}")
+        end
+      end
+    end
   end
   
   def run_suites
