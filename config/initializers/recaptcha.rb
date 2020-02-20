d12d156a851f53a8ed1e933de1b9bef6eef4b4b6
susmitha9542 <susmitha.tinku@gmail.com>
2020-02-20 12:20:56 -0500

    trying to fix recaptcha tag with v2

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,5 @@
 Recaptcha.configure do |config|
-config.site_key = '6LdUvdoUAAAAAHMKJk73RQjqAkucQKzFurX4xsPp'
-config.secret_key = '6LdUvdoUAAAAAIJUxXIpzXAH-qvWNT2cNNf6Xc1U'
+config.site_key = '6LcdydoUAAAAANWyMeMLDzm5Gdc9rgL9_XL6yK0C'
+config.secret_key = '6LcdydoUAAAAALB3rlYnsIt5lZ1OjxY7sJp-W_bU'
 config.proxy = 'http://www.google.com/recaptcha/api/verify'
 end
