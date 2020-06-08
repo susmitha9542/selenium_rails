74023513466c8ffe7a560872a2a4becaf588a3da
susmitha9542 <susmitha.3757@gmail.com>
2020-06-08 04:36:59 -0400

    fix for environments not creating

--- app/controllers/environments_controller.rb
+++ app/controllers/environments_controller.rb
@@ -1,4 +1,5 @@
 class EnvironmentsController < ApplicationController
+  skip_before_action :verify_authenticity_token
   before_action :set_environment, only: [:show, :edit, :update, :destroy]
 
   # GET /environments
